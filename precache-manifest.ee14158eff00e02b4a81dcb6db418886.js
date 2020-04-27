65e46c797ee32052b6418a7c068622b56d39fcd0
Ethan Scott <ethanscott@ucsb.edu>
2020-04-27 15:39:57 -0700

    Updates

---
+++ precache-manifest.ee14158eff00e02b4a81dcb6db418886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5743800564dafba508266ab029758763",
+    "url": "/cs185_react/index.html"
+  },
+  {
+    "revision": "8c87b703cbc56e4aa627",
+    "url": "/cs185_react/static/css/main.cc67f5d5.chunk.css"
+  },
+  {
+    "revision": "e5dfb04c43a678efd107",
+    "url": "/cs185_react/static/js/2.15b06700.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cs185_react/static/js/2.15b06700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c87b703cbc56e4aa627",
+    "url": "/cs185_react/static/js/main.051809a9.chunk.js"
+  },
+  {
+    "revision": "be51dbb9a70355aa78f3",
+    "url": "/cs185_react/static/js/runtime-main.3b751172.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs185_react/precache-manifest.318d35d7d946c9d52c60d5b46f1c9710.js"
+  "/cs185_react/precache-manifest.ee14158eff00e02b4a81dcb6db418886.js"
 );
 
 self.addEventListener('message', (event) => {
