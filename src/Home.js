29fb903322bb951f6194d9d3fe7a7c762826a66a
Ethan Scott <ethanscott@ucsb.edu>
2020-04-27 16:44:13 -0700

    es - overhauled linking and overall structure

--- src/Home.js
+++ src/Home.js
@@ -8,14 +8,6 @@ class Home extends Component{
     render(){
         return (
             <div>
-            <div id="nav">
-                <u1>
-                    <li ><a class="current" href="/cs185_react/index">Home</a></li>
-                    <li ><a href="/cs185_react/images">Images</a></li>
-                    <li ><a href="/cs185_react/videos">Videos</a></li>
-                    <li ><a href="/cs185_react/links">Links</a></li>
-                </u1>
-            </div>
             <br></br>
             <div>
                 <ScrollUpButton />
@@ -25,7 +17,7 @@ class Home extends Component{
                 I'm currently in my second year. I hope to learn more about website design and user 
                 interfaces in this class. </p>
             <br></br>
-            <img src="/images/ucsb.jpg" height="100" width="130"></img>
+            <img src="./images/ucsb.jpg" height="100" width="130"></img>
             </div>
         )
     }
--- src/Images.js
+++ src/Images.js
@@ -15,7 +15,7 @@ class Images extends Component{
     }
     
     openLightBox(a){
-        var imgsrc = "/images/" + a + ".jpg";
+        var imgsrc = "../images/" + a + ".jpg";
         console.log(imgsrc);
         this.setState({
             "enableLB": true,
@@ -35,29 +35,21 @@ class Images extends Component{
         if (!this.state.enableLB){
         return(
             <div>
-                <div id="nav">
-                    <u1>
-                        <li><a href="/cs185_react/index">Home</a></li>
-                        <li><a class="current" href="/cs185_react/images">Images</a></li>
-                        <li><a href="/cs185_react/videos">Videos</a></li>
-                        <li><a href="/cs185_react/links">Links</a></li>
-                    </u1>
-                </div>
                 <br></br>
                 <h3 id="imgTitle"> Summer 2019, Images </h3>
                 <div>
                     <ScrollUpButton />
                 </div>
                 <div id="pictureGrid" class="container">
-                        <img id="river" src="/images/river.jpg" onClick={() => this.openLightBox('river')} />
-                        <img id="whitney" src="/images/whitney.jpg" onClick={() => this.openLightBox('whitney')} />
-                        <img id="rocks" src="/images/rocks.jpg" onClick={() => this.openLightBox('rocks')} />
-                        <img id="lake" src="/images/lake.jpg" onClick={() => this.openLightBox('lake')} />
-                        <img id="sunset" src="/images/sunset.jpg" onClick={() => this.openLightBox('sunset')} />
-                        <img id="trees" src="/images/trees.jpg" onClick={() => this.openLightBox('trees')} />
-                        <img id="canyon" src="/images/canyon.jpg" onClick={() => this.openLightBox('canyon')} />
-                        <img id="mountain" src="/images/mountain.jpg" onClick={() => this.openLightBox('mountain')} />
-                        <img id="tent" src="/images/tent.jpg" onClick={() => this.openLightBox('tent')} />
+                        <img id="river" src="./images/river.jpg" onClick={() => this.openLightBox('river')} />
+                        <img id="whitney" src="./images/whitney.jpg" onClick={() => this.openLightBox('whitney')} />
+                        <img id="rocks" src="./images/rocks.jpg" onClick={() => this.openLightBox('rocks')} />
+                        <img id="lake" src="./images/lake.jpg" onClick={() => this.openLightBox('lake')} />
+                        <img id="sunset" src="./images/sunset.jpg" onClick={() => this.openLightBox('sunset')} />
+                        <img id="trees" src="./images/trees.jpg" onClick={() => this.openLightBox('trees')} />
+                        <img id="canyon" src="./images/canyon.jpg" onClick={() => this.openLightBox('canyon')} />
+                        <img id="mountain" src="./images/mountain.jpg" onClick={() => this.openLightBox('mountain')} />
+                        <img id="tent" src="./images/tent.jpg" onClick={() => this.openLightBox('tent')} />
                 </div>
             </div>
         )
