9ac9d5b545b26aa77f8953dac20f6412e2cfd02f
Ethan Scott <ethanscott@ucsb.edu>
2020-04-27 11:54:16 -0700

    Updates

---
+++ precache-manifest.f4a351ba8aff3f5afc6eca76cfabb99d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930b756eedb5bbb0c6fd9a9b6e005af6",
+    "url": "/cs185_react/index.html"
+  },
+  {
+    "revision": "f990a511168b5fa43412",
+    "url": "/cs185_react/static/css/main.cc67f5d5.chunk.css"
+  },
+  {
+    "revision": "f115edc9e370ff20b41e",
+    "url": "/cs185_react/static/js/2.987548fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cs185_react/static/js/2.987548fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f990a511168b5fa43412",
+    "url": "/cs185_react/static/js/main.5859738a.chunk.js"
+  },
+  {
+    "revision": "be51dbb9a70355aa78f3",
+    "url": "/cs185_react/static/js/runtime-main.3b751172.js"
+  }
+]);
