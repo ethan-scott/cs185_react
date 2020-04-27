7b2d85e4c9d3ead708b5b5ea90cf9673e163f401
Ethan Scott <ethanscott@ucsb.edu>
2020-04-27 10:52:00 -0700

    es - added lightbox functionality for videos

--- src/Videos.js
+++ src/Videos.js
@@ -3,8 +3,25 @@ import ReactDOM from "react-dom";
 import './index.css';
 
 class Videos extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            "vidnum": 0
+        }
+        this.closeLightBox = this.closeLightBox.bind(this);
+    }
+
+    openLightBox(a){
+        this.setState({"vidnum": a})
+    }
+    
+    closeLightBox(){
+        this.setState({"vidnum": 0})
+    }
+
     render(){
-        return(
+        if (this.state.vidnum == 1){
+            return(
             <div>
                 <div id="nav">
                 <u1>
@@ -15,28 +32,62 @@ class Videos extends Component{
                 </u1>
                 </div>
                 <br/>
-                <button id="scrollTop" title="Go to top">Top</button>
-                <h3> Summer 2019, Videos </h3>
-                <div id="videoGrid" class="container">
-                    <video width="320" height="240" controls>
-                        <source src="/images/vid1.mp4" type="video/MP4" />
-                    </video>
-                    <video width="320" height="240" controls>
-                        <source src="/images/vid3.mp4" type="video/MP4" />
-                    </video>
+                <div class="close">
+                    <button onClick={this.closeLightBox}>Close LightBox</button>
                 </div>
-                <div id="LightBox1" class="LBox">
+                <div id="LightBox1" class="LBoxV">
                     <video id="LBvid1" width="500" height = "380" controls>
                         <source src="/images/vid1.mp4" type="video/MP4" />
                     </video>        
                 </div>
-                <div id="LightBox3" class="LBox">
-                    <video id="LBvid3" width="500" height = "380" controls>
-                        <source src="/images/vid3.mp4" type="video/MP4" />
-                    </video>        
-                </div>
             </div>
-        )
+            )
+        } else if (this.state.vidnum == 3){
+            return(
+                <div>
+                    <div id="nav">
+                    <u1>
+                        <li><a href="/index">Home</a></li>
+                        <li><a href="/images">Images</a></li>
+                        <li><a class="current" href="/videos">Videos</a></li>
+                        <li><a href="/links">Links</a></li>
+                    </u1>
+                    </div>
+                    <br/>
+                    <div class="close">
+                        <button onClick={this.closeLightBox}>Close LightBox</button>
+                    </div>
+                    <div id="LightBox3" class="LBoxV">
+                        <video id="LBvid3" width="500" height = "380" controls>
+                            <source src="/images/vid3.mp4" type="video/MP4" />
+                        </video>        
+                    </div>
+                </div>
+                )
+        }else{
+            return(
+                <div>
+                    <div id="nav">
+                    <u1>
+                        <li><a href="/index">Home</a></li>
+                        <li><a href="/images">Images</a></li>
+                        <li><a class="current" href="/videos">Videos</a></li>
+                        <li><a href="/links">Links</a></li>
+                    </u1>
+                    </div>
+                    <br/>
+                    <h3> Summer 2019, Videos </h3>
+                    <div id="videoGrid" class="container">
+                        <video onClick={() => this.openLightBox(1)} width="320" height="240" controls>
+                            <source src="/images/vid1.mp4" type="video/MP4" />
+                        </video>
+                        <video onClick={() => this.openLightBox(3)} width="320" height="240" controls>
+                            <source src="/images/vid3.mp4" type="video/MP4" />
+                        </video>
+                    </div>
+                </div>
+            )
+        }
     }
 }
 
