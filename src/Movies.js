426c2b01bc67a0eb7256a588fa66adfd1ee44057
Ethan Scott <ethanscott@ucsb.edu>
2020-05-16 15:59:47 -0700

    es - added lightbox and stopped scroll

--- src/Movies.js
+++ src/Movies.js
@@ -8,12 +8,15 @@ class Movies extends Component{
         this.state = {
             "getMovies": true,
             "movies":[],
+            "moviesrc": {title: "", poster: "", director: "", rating: ""},
+            "enableLB": false,
             "IDs":["tt1375666", "tt0816692",
                    "tt0114369", "tt0407887",
                    "tt0062622", "tt0477348",
                    "tt1392214", "tt2582782"]
         }
         this.getMovies = this.getMovies.bind(this);
+        this.openLightBox = this.openLightBox.bind(this);
     }
 
     getMovies(){
@@ -36,15 +39,51 @@ class Movies extends Component{
         this.setState({"getMovies": false});
     }
 
+    openLightBox(a){
+        document.documentElement.style.overflow = 'hidden';
+        document.body.scroll = "no";
+        console.log(a);
+        this.setState({"moviesrc": a, "enableLB": true});
+        console.log(this.state.moviesrc);
+    }
+
+    closeLightBox(){
+        document.documentElement.style.overflow = 'scroll';
+        document.body.scroll = "yes";
+        this.setState({"enableLB": false});
+    }
+
     render(){
         if(this.state.getMovies){
             this.getMovies();
         }
+        if(!this.state.enableLB){
+            return(
+                <div>
+                    <div class="MovieGrid">
+                        {this.state.movies.map((m) =>(
+                            <img src={m.poster} onClick={() => this.openLightBox(m)}></img>
+                        ))}
+                    </div>
+                </div>
+            )
+        }
         return(
-            <div class="MovieGrid">
-                {this.state.movies.map((m) =>(
-                    <img src={m.poster}></img>
-                ))}
+            <div>
+                <div class="MovieGridOpaque">
+                        {this.state.movies.map((m) =>(
+                            <img src={m.poster}></img>
+                        ))}
+                </div>
+                <div class="MovieLB">
+                    <img src= {this.state.moviesrc.poster}></img>
+                    <div class="MovieDescription">
+                        <p>&ensp; {this.state.moviesrc.title}</p>
+                        <p>&ensp; Director: {this.state.moviesrc.director}</p>
+                        <p>&ensp; Rating: {this.state.moviesrc.rating}</p>
+                        <button onClick={() => this.closeLightBox()} >Close</button>
+                    </div>
+                </div>
             </div>
         )
     }
